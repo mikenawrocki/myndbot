6f70f9e821d57e9e36f23417ed4b5fb49b7a9155
Mike Nawrocki <mnawrocki3@gatech.edu>
2013-09-12 20:29:24 -0400

    Initializing Repository.

---
+++ irclog.py
@@ -0,0 +1,71 @@
+import pymongo
+
+class BotLogger(object):
+    def __init__(self, handlers = [], enabled = True):
+        self.enabled = enabled
+        self.handlers = handlers
+
+    def enable_logging(self):
+        self.enabled = True
+
+    def disable_logging(self):
+        self.enabled = False
+
+    def add_handler(self, handler):
+        if not isinstance(handler, Handler):
+            raise ValueError("Attempt to add non-handler!")
+
+        self.handlers.append(handler)
+
+    def remove_handler(self, handler):
+        if handler not in self.handlers:
+            return
+
+        handler.close()
+        self.handlers.remove(handler)
+
+    def log(self, msg, date, source, msg_type):
+        if not self.enabled:
+            return
+
+        for handler in self.handlers:
+            handler.log(msg, date, source, msg_type)
+
+
+class Handler(object):
+    def log(self, msg, date, source, msg_type):
+        raise NotImplementedError("Abstract Method")
+
+    def close(self):
+        pass
+
+class MongoHandler(Handler):
+    def __init__(self, server='localhost',
+                       database='irc',
+                       collection='log',
+                       port = 27017):
+
+        self.client = pymongo.MongoClient(server, int(port))
+        self.db = self.client[database]
+        self.collection = collection
+
+    def log(self, msg, date, source, msg_type):
+        self.db[self.collection].insert({
+            "date" : date,
+            "type" : msg_type,
+            "content" : msg,
+            "source" : source
+        })
+
+    def close(self):
+        self.client.close()
+
+class StdoutHandler(Handler):
+    def log(self, msg, date, source, msg_type):
+        print("{}|{}|{}|{}".format(date,msg_type,source,msg))
+
+class NullHandler(Handler):
+    def log(self, msg, date, source, msg_type):
+        pass
+
+# vim: ts=8 sts=4 sw=4 et
---
+++ myndbot.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import bson
+import configparser
+import datetime
+import irc.bot
+import irc.strings
+import irclog
+import pymongo
+
+logger = None
+
+class MyndBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, channel, nickname, server, port=6667, ns_pass=None):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server,port)], nickname,
+                                            nickname)
+        self.channel = channel
+        self.nickname = nickname
+        self.ns_pass = ns_pass
+
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e): 
+        if self.ns_pass:
+            self.authenticate()
+
+        c.join(self.channel)
+
+    def on_ctcp(self, c, e):
+        msg = e.arguments[1]
+        date = datetime.datetime.utcnow()
+        source = e.source.nick
+        logger.log(msg, date, source, "ctcp")
+
+
+    def on_pubmsg(self, c, e):
+        msg = e.arguments[0]
+        date = datetime.datetime.utcnow()
+        source = e.source.nick
+        logger.log(msg, date, source, "msg")
+
+        cmd_sentinel = '!'
+        if msg.startswith(cmd_sentinel):
+            self.do_command(e, msg[len(cmd_sentinel):].strip().split(' '))
+
+    def authenticate(self):
+        self.connection.notice("nickserv", "identify {}".format(ns_pass))
+
+    def is_admin(self, nick):
+        chan_obj = self.channels[self.channel]
+        if nick in chan_obj.opers() or \
+           nick in chan_obj.halfops() or \
+           nick in chan_obj.owners():
+            return True
+
+        return False
+
+    def do_command(self, e, cmd):
+        nick = e.source.nick
+        c = self.connection
+
+        if cmd[0] == "die":
+            if not self.is_admin(nick):
+                c.notice(nick, "You cannot tell me to die!")
+            else:
+                self.die(msg="BAI BAI")
+
+        elif cmd[0] == "kick":
+            if not self.is_admin(nick):
+                c.notice(nick, "I will not kick for you.")
+            else:
+                c.kick(self.channel, cmd[1])
+
+        else:
+            c.notice(nick, "Not understood: " + ' '.join(cmd))
+
+def setup_logger(config):
+    global logger
+    log_name = config.get('logging', 'logger', fallback="Null")
+    log_handler_name = "{}Handler".format(log_name.capitalize())
+    try:
+        log_handler_type = getattr(irclog, log_handler_name)
+    except AttributeError:
+        raise RuntimeError("No handler for type {}, aborting!".format(
+                                                        log_handler_name))
+    try:
+        log_opts = dict(config.items(log_name))
+    except configparser.NoSectionError:
+        log_opts = {}
+    log_handler = log_handler_type(**log_opts)
+    logger = irclog.BotLogger()
+    logger.add_handler(log_handler)
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('myndconfig.ini')
+    
+    setup_logger(config)
+
+    if not config.has_option('irc', 'server'):
+        raise RuntimeError("No server provided! Aborting...")
+    if not config.has_option('irc', 'channel'):
+        raise RuntimeError("No channel provided! Aborting...")
+
+    srv = config.get('irc', 'server')
+    chan = config.get('irc', 'channel')
+    nick = config.get('irc', 'nickname', fallback='myndbot')
+    port = config.getint('irc', 'port', fallback=6667)
+
+    bot = MyndBot(chan, nick, srv, port=port)
+    bot.start()
+
+if __name__ == "__main__":
+    main()
+
+# vim: ts=8 sts=4 sw=4 et
